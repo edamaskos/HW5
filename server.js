f762b2f9cddc8207726b522a3a10318632e563db
Elena <elena.damaskos@yale.edu>
2016-10-05 17:25:47 +0000

    add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/funny-porcupine', function(request, response){
+    response.send('well hello there!');
+});
+
+
 app.listen(process.env.PORT || 4000);
